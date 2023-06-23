e71ddcd2b8bd823a158f1c694fb7a30213749cc5
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-06-23 16:08:36 -0300

    Generic e List

--- Array_Collections_C-aula02/bytebank_ATENDIMENTO/bytebank.Modelos/Conta/Cliente.cs
+++ Array_Collections_C-aula02/bytebank_ATENDIMENTO/bytebank.Modelos/Conta/Cliente.cs
@@ -3,7 +3,7 @@
     public class Cliente
     {
 
-        public string Cpf { get; set; }
+        public string? Cpf { get; set; }
 
         private string _nome;
         public string Nome
@@ -22,7 +22,7 @@
 
         }
         //public string Nome { get; set; }
-        public string Profissao { get; set; }
+        public string? Profissao { get; set; }
 
         public static int TotalClientesCadastrados { get; set; }
 
